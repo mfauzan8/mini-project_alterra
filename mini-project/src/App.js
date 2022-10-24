edd399df04e149bc0ae7ecd8be98375b9a387d1c
mfauzan8 <kerabatfauzan@gmail.com>
2022-10-24 21:17:04 +0700

    feat: add react-router-dom

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./Components/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
