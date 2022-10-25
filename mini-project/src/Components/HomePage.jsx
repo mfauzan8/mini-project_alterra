f04e0cf61c94a30b8206f901b5f2938ea7321e6d
mfauzan8 <kerabatfauzan@gmail.com>
2022-10-26 01:27:11 +0700

    fix: display homepage

--- mini-project/src/Components/HomePage.jsx
+++ mini-project/src/Components/HomePage.jsx
@@ -1,31 +1,30 @@
 import React from "react";
-import ImageHome from "../Assets/img/ImageHome.png";
 import { Link } from "react-router-dom";
-import { Row, Col, Container, Image, Button, Stack } from "react-bootstrap";
-import LogoHome from "../Assets/img/LogoHome.png";
-import "../Assets/css/home.css";
+import { Container, Image, Stack, Col } from "react-bootstrap";
 
 const HomePage = () => {
   return (
     <div className="home">
-      <Container fluid>
-        <Stack direction="horizontal">
-          <Stack gap={4}>
-            <div className="logoHome">
-              <Image src={LogoHome} />
-            </div>
-
-            <div className="TextHeader">
-              MEET, <span>EAT</span> & <span>ENJOY</span> THE GOOD <span>TASTE</span>
-            </div>
-            <div className="TextSubHeader">We deliver 100% Fresh Food and drink. You can order right now!</div>
-            <Link to={"/menu"}>
-              <Button className="btnOurmenu">OUR MENU</Button>
-            </Link>
-          </Stack>
-          <Image src={ImageHome} className="imageHome" />
+      <Stack direction="horizontal">
+        <Stack>
+          <Image src="/Assets/img/LogoHome.png" style={{ width: "18%", padding: "30px" }} />
+          <Container>
+            <Col md={8} style={{ padding: "3rem" }}>
+              <div className="TextHeader">
+                MEET, <span>EAT</span> & <span>ENJOY</span> THE GOOD <span>TASTE</span>
+              </div>
+              <div className="TextSubHeader">We deliver 100% Fresh Food and drink. You can order right now!</div>
+              <Link to={"/menu"}>
+                <button className="btnOurmenu">
+                  OUR MENU <i class="bi bi-book-half"></i>
+                </button>
+              </Link>
+            </Col>
+          </Container>
         </Stack>
-      </Container>
+
+        <Image src="/Assets/img/ImageHome.png" className="h-100 position-absolute end-0 top-0" />
+      </Stack>
     </div>
   );
 };
