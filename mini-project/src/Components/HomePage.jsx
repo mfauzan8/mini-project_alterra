3584ba9e93a5436442d373d28a5a724058197209
mfauzan8 <mhmmdfauzan296@gmail.com>
2022-10-25 16:22:32 +0700

    feat: add Home Page

--- mini-project/src/Components/HomePage.jsx
+++ mini-project/src/Components/HomePage.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+import ImageHome from "../Assets/img/ImageHome.png";
+import { Link } from "react-router-dom";
+import { Row, Col, Container, Image, Button, Stack } from "react-bootstrap";
+import LogoHome from "../Assets/img/LogoHome.png";
 import "../Assets/css/home.css";
 
 const HomePage = () => {
   return (
-    <div className="d-flex mb-3 justify-content-around">
-      <div className="p-2">Flex item 1</div>
-      <div className="p-2">
-        <Image src="./Assets/img/ImageHome.png" />
-      </div>
+    <div className="home">
+      <Container fluid>
+        <Stack direction="horizontal">
+          <Stack gap={4}>
+            <div className="logoHome">
+              <Image src={LogoHome} />
+            </div>
+
+            <div className="TextHeader">
+              MEET, <span>EAT</span> & <span>ENJOY</span> THE GOOD <span>TASTE</span>
+            </div>
+            <div className="TextSubHeader">We deliver 100% Fresh Food and drink. You can order right now!</div>
+            <Link to={"/menu"}>
+              <Button className="btnOurmenu">OUR MENU</Button>
+            </Link>
+          </Stack>
+          <Image src={ImageHome} className="imageHome" />
+        </Stack>
+      </Container>
     </div>
   );
 };
---
+++ mini-project/src/Containers/AllProductPageContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AllProductPage from "../Components/AllProductPage";
+
+const AllProductPageContainer = () => {
+  return <AllProductPage />;
+};
+
+export default AllProductPageContainer;
